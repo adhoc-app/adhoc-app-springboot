0c23c5004a1c3fe2a2d8d843ffb8d6e42cf2270e
Huyen Truong <huyen@free.fi>
2021-05-31 21:50:12 +0300

    first commit

---
+++ adhoc-app-springboot/src/main/java/fi/adhocapp/adhocappspringboot/AdhocAppSpringbootApplication.java
@@ -0,0 +1,13 @@
+package fi.adhocapp.adhocappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhocAppSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdhocAppSpringbootApplication.class, args);
+	}
+
+}
